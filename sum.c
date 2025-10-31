428340829bbbaa662216d42272e80051c00cce50
gokul-s-kammath <gokul.s.1551@gmail.com>
2025-10-31 23:41:31 +0530

    Update and rename README.md to sum.c

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+int num1,num2,sum;
+printf("Enter a number:");
+scanf("%d",&num1);
+printf("Enter another number:");
+scanf("%d",&num2);
+sum=num1+num2;
+printf("The sum is %d",sum);
+return 0;
+
+}
