2501b5c918e91310cd110244b1971c540db022b1
gokul-s-kammath <gokul.s.1551@gmail.com>
2025-11-01 18:15:08 +0530

    Create zero.c

---
+++ zero.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf ("Enter a number :\n");
+scanf("%d",&num);
+if(num<0)
+{
+printf("The number is negative");
+}
+else if(num>0)
+{
+printf("The number is positive");
+}
+else
+{
+printf("The number is zero");
+}
+return 0;
+}
