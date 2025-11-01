8d1fc2f688f0c0bc14d1c115977dc60f55fa162c
gokul-s-kammath <gokul.s.1551@gmail.com>
2025-11-01 18:23:55 +0530

    Create division.c

---
+++ division.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int n,d,div;
+printf("Enter the numerator:");
+scanf("%d",&n);
+printf("Enter the denominator:");
+scanf("%d",&d);
+div=n/d;
+printf("The answer is %d ",div);
+return 0;
+}
