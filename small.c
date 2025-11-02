1697fd1a28045cafa6850de8c60a80aa5471a216
gokul-s-kammath <gokul.s.1551@gmail.com>
2025-11-02 08:43:35 +0530

    Update and rename large.c to small.c

--- small.c
+++ small.c
@@ -7,8 +7,8 @@ scanf("%d",&num1);
 printf("Enter another number:");
 scanf("%d",&num2);
 if(num1>num2)
-printf("The %d is larger",num1);
+printf("The %d is smaller",num2);
 else
-printf("The %d is larger",num2);
+printf("The %d is samller",num1);
 return 0;
 }
