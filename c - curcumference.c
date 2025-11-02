2dc84c7c9029b710ba7df9c8ddc2c314e30afe73
gokul-s-kammath <gokul.s.1551@gmail.com>
2025-11-02 09:00:27 +0530

    Create c - curcumference.c

---
+++ c - curcumference.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+float rad,cercum,pi=3.14;
+
+printf("Enter a radius:");
+scanf("%f",&rad);
+cercum= 2*pi*rad;
+printf("The circumference is:%f",cercum);
+return 0;
+}
