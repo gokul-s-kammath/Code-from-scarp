aab281cb5842750430cfde060f371a0bb2bc0153
gokul-s-kammath <gokul.s.1551@gmail.com>
2025-11-02 09:18:44 +0530

    Create exponent.c

---
+++ exponent.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int num;
+double power;
+printf("Enter a number:");
+scanf("%d",&num);
+power= pow(num,2);
+printf("The square of the numbers is:%f",power);
+return 0;
+}
